accdb81c4c15a78809a3106afe939f8f775d6c67
fabeat <fg@linkable.de>
2010-05-21 20:38:42 +0200

    added missing require_once

--- curl.php
+++ curl.php
@@ -2,4 +2,4 @@
 
 require_once 'lib'.DIRECTORY_SEPARATOR.'curl.php';
 require_once 'lib'.DIRECTORY_SEPARATOR.'curl_response.php';
-require_once 'lib'.DIRECTORY_SEPARATOR.'curl_exception.php';
+require_once 'lib'.DIRECTORY_SEPARATOR.'curl_put_data.php';
