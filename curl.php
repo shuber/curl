500dbd7a3c753a5d9ffb7084e749daa7b463abd2
Fabian Graßl <fg@linkable.de>
2011-04-08 19:36:34 +0200

    includes

--- curl.php
+++ curl.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once 'lib'.DIRECTORY_SEPARATOR.'curl.php';
-require_once 'lib'.DIRECTORY_SEPARATOR.'curl_response.php';
-require_once 'lib'.DIRECTORY_SEPARATOR.'curl_put_data.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'curl.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'curl_response.php';
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'curl_put_data.php';
