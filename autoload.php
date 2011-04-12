f00a2dde072e9a8e01aa811be5e082567b036680
Fabian Graßl <fg@linkable.de>
2011-04-12 13:25:20 +0200

    added very simple autoloader

--- autoload.php
+++ autoload.php
@@ -1,5 +1,21 @@
 <?php
 
-require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'curl.php';
-require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'curl_response.php';
-require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'curl_put_data.php';
+function curl_autoload($className)
+{
+  $prefix = dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR;
+  switch ($className)
+  {
+    case 'Curl':
+      require_once $prefix.'curl.php';
+      break;
+    case 'CurlResponse':
+      require_once $prefix.'curl_response.php';
+      break;
+    case 'CurlPutData':
+      require_once $prefix.'curl_put_data.php';
+      break;
+  }
+}
+
+spl_autoload_register('curl_autoload');
+
--- curl.php
+++ curl.php
@@ -1,20 +1,3 @@
 <?php
 
-function curl_autoload($className)
-{
-  $prefix = dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR;
-  switch ($className)
-  {
-    case 'Curl':
-      require_once $prefix.'curl.php';
-      break;
-    case 'CurlResponse':
-      require_once $prefix.'curl_response.php';
-      break;
-    case 'CurlPutData':
-      require_once $prefix.'curl_put_data.php';
-      break;
-  }
-}
-
-spl_autoload_register('curl_autoload');
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'autoload.php';
